bff4d15e74a522866a8e5023dc05c353339fb7ac
xolmasek005 <masekox005@gmail.com>
2023-08-16 10:14:45 +0200

    Cars Classified

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def main_view(request):
+    return render(request, 'views/main.html', {'name': 'Cars Classified'})
